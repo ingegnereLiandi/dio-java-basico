229aa28ef6be99c0c30b26ec1d934296131404a9
Liandi <douglasliandi@hotmail.com>
2023-05-17 17:26:47 +0200

    Alterando msg de impressao

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Ciao, Raga!");
+        System.out.println("Io sono bravo!");
     }
 }
